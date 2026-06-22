4a6acf4b32d817c6d26f147dbcc69cc6bcca193e
YuriyGl86 <yuriygl86@gmail.com>
2026-06-22 09:22:30 +0300

    infra

--- stats-service/stats-client/src/main/java/ru/practicum/stat/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/stat/client/StatsClient.java
@@ -51,7 +51,7 @@ public class StatsClient {
                 .path("/stats")
                 .queryParam("start", paramDto.getStart().format(FORMATTER))
                 .queryParam("end", paramDto.getEnd().format(FORMATTER))
-                .queryParam("uris", paramDto.getUris()) // убрал (Object). Из-за этого не работала статистика
+                .queryParam("uris", (Object[]) paramDto.getUris())
                 .queryParam("unique", paramDto.getUnique())
                 .build()
                 .encode()
