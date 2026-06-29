d5ec056af923cd9ae3679ebbdd03411c0cc76d3a
Yuriy Glushchenko <yuriygl86@gmail.com>
2026-06-29 19:59:09 +0300

    requst, comment, names

--- core/event-service/src/main/java/ru/practicum/eventsService/client/CommentClient.java
+++ core/event-service/src/main/java/ru/practicum/eventsService/client/CommentClient.java
@@ -3,6 +3,6 @@ package ru.practicum.eventsService.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.common.apiContracts.CommentsApiContract;
 
-@FeignClient(name = "comments-service")
+@FeignClient(name = "comment-service")
 public interface CommentClient extends CommentsApiContract {
 }
--- core/event-service/src/main/java/ru/practicum/eventsService/client/ParticipationRequestClient.java
+++ core/event-service/src/main/java/ru/practicum/eventsService/client/ParticipationRequestClient.java
@@ -3,6 +3,6 @@ package ru.practicum.eventsService.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.common.apiContracts.ParticipationRequestApiContract;
 
-@FeignClient(name = "requests-service")
+@FeignClient(name = "request-service")
 public interface ParticipationRequestClient extends ParticipationRequestApiContract {
 }
